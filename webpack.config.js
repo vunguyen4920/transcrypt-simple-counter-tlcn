ef40abde56443bc4234c24ea690386b7a989a6cc
Vu Nguyen <vunguyen4920@gmail.com>
2021-11-05 11:59:24 +0700

    Rework the issue

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,7 @@
-const CopyPlugin = require("copy-webpack-plugin");
-const path = require("path");
-
 module.exports = {
   devtool: "inline-source-map",
   entry: "./app.py",
   mode: "development",
-  output: {
-    path: path.join(__dirname, "/dist"),
-    filename: "bundle.js",
-  },
   module: {
     rules: [
       {
@@ -18,16 +11,4 @@ module.exports = {
       },
     ],
   },
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "dist"),
-    },
-    compress: true,
-    port: 5000,
-  },
-  plugins: [
-    new CopyPlugin({
-      patterns: [{ from: "public" }],
-    }),
-  ],
 };
